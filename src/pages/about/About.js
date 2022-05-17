6dc66e867ddf0199ad8f86e1c921b3f016eb3035
aycadogan89 <doganayca89@gmail.com>
2022-05-17 10:31:22 -0700

    issues

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './About.scss'
 import {
   faNode,
@@ -10,51 +10,54 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
 const About = () => {
-  
-  
   return (
-   
-    <div className='full-page about-page' id="about">
+    <div className='full-page about-page' id='about'>
       <div className='text-zone'>
-          <h2 className='padding-bottom'>
-            About Me
-          </h2>
-          <h4 className='text-pink'>Who I am</h4>
-          <p className='para'>
-            Hi!I am Ayca Dogan. I'm currently learning web development in CICC. Having a master degree in Digital Solutions Development from Barcelona Technology School involved me in creating <span className='dancing-script'>engaging</span> and <span className="dancing-script">user friendly</span> web experiences. 
-          </p>
-          <h4 className='text-pink'>Skills & Tools</h4>
-          <p><strong>Languages:</strong>  JavaScript, HTML, CSS</p> 
-          <p><strong>Frameworks:</strong>  React, NodeJS, Express, Bootstrap</p>
-          <p><strong>Databases&Tools:</strong>  MongoDB, MySQL, Firebase, UXPin, Postman, Git</p>
-        </div>
-      <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faNode} color="#43853D" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-            </div>
+        <h2 className='padding-bottom'>About Me</h2>
+        <h4 className='text-pink'>Who I am</h4>
+        <p className='para'>
+          Hi!I am Ayca Dogan. I'm currently learning web development in CICC.
+          Having a master degree in Digital Solutions Development from Barcelona
+          Technology School involved me in creating{' '}
+          <span className='dancing-script'>engaging</span> and{' '}
+          <span className='dancing-script'>user friendly</span> web experiences.
+        </p>
+        <h4 className='text-pink'>Skills & Tools</h4>
+        <p>
+          <strong>Languages:</strong> JavaScript, HTML, CSS
+        </p>
+        <p>
+          <strong>Frameworks:</strong> React, NodeJS, Express, Bootstrap
+        </p>
+        <p>
+          <strong>Databases&Tools:</strong> MongoDB, MySQL, Firebase, UXPin,
+          Postman, Git
+        </p>
+      </div>
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+          <div className='face1'>
+            <FontAwesomeIcon icon={faNode} color='#43853D' />
+          </div>
+          <div className='face2'>
+            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+          </div>
+          <div className='face3'>
+            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+          </div>
+          <div className='face4'>
+            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+          </div>
+          <div className='face5'>
+            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+          </div>
+          <div className='face6'>
+            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
           </div>
         </div>
+      </div>
     </div>
-   
-
   )
 }
 
